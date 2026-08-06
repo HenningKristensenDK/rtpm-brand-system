d9dc46dfdf6bcc94654418df07daa333f551552d
Henning Kristensen <henning@rtpm.dk>
2026-08-06 21:39:31 +0200

    Add package.json, build tooling, and barrel entry for design-sync

---
+++ index.ts
@@ -0,0 +1,8 @@
+export { Button } from './components/Button';
+export type { ButtonProps } from './components/Button';
+
+export { StatusPill } from './components/StatusPill';
+export type { StatusPillProps, StatusLevel } from './components/StatusPill';
+
+export { StatusStepper } from './components/StatusStepper';
+export type { StatusStepperProps, Step, StepState } from './components/StatusStepper';
